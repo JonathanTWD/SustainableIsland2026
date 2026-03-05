0a02f918087e9f6948979ac27cb905ead7da1ac3
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-03-05 12:45:05 +0100

    everything should be working again

--- frontend/src/pages/SignUpPageView.tsx
+++ frontend/src/pages/SignUpPageView.tsx
@@ -1,52 +1,52 @@
-import Button from "../components/Button";
-import InputField from "../components/InputField";
-import ToggleSwitch from "../components/ToggleSwitch";
+import Button from "../component/LoginSignInComponents/Button";
+import InputField from "../component/LoginSignInComponents/InputField";
+import ToggleSwitch from "../component/LoginSignInComponents/ToggleSwitch";
 
 
 const SignupPage = () => {
     return (
         <>
-        <div className="px-6 py-15 flex flex-col gap-12">
-            {/* Welcome banner */}
-            <h1 className="text-[#0A302F] font-kalam font-bold text-[40px]">
-                Welcome
-            </h1>
+            <div className="px-6 py-15 flex flex-col gap-12">
+                {/* Welcome banner */}
+                <h1 className="text-[#0A302F] font-kalam font-bold text-[40px]">
+                    Welcome
+                </h1>
 
-            {/* Input fields */}
-            <div className="flex flex-col gap-6">
-                <div className="flex justify-center">
-                    <span className="text-[#0A302F] font-nunito text-[24px] font-bold ">
-                        Create account
-                    </span>
-                </div>
-                <InputField type="text" placeholder="Username" />
-                <InputField type="text" placeholder="Email" />
-                <InputField type="password" placeholder="Password" />
-            </div >
+                {/* Input fields */}
+                <div className="flex flex-col gap-6">
+                    <div className="flex justify-center">
+                        <span className="text-[#0A302F] font-nunito text-[24px] font-bold ">
+                            Create account
+                        </span>
+                    </div>
+                    <InputField type="text" placeholder="Username" />
+                    <InputField type="text" placeholder="Email" />
+                    <InputField type="password" placeholder="Password" />
+                </div >
 
-            {/* Buttons */}
-            <div className="flex-col gap-2.5">
-                <Button text="Sign up" onClick={() => {}} 
+                {/* Buttons */}
+                <div className="flex-col gap-2.5">
+                    <Button text="Sign up" onClick={() => { }}
                     />
-                <div className="flex items-center gap-5 my-2.5">
-                    <div className="grow border-t border-[#0A302F] "></div>
-                    <span className="text-[#0A302F] font-kalam text-[20px]">
-                        Or
-                    </span>
-                    <div className="grow border-t border-[#0A302F]"></div>
-                </div>
-                <Button text="Log in" onClick={() => {}} 
+                    <div className="flex items-center gap-5 my-2.5">
+                        <div className="grow border-t border-[#0A302F] "></div>
+                        <span className="text-[#0A302F] font-kalam text-[20px]">
+                            Or
+                        </span>
+                        <div className="grow border-t border-[#0A302F]"></div>
+                    </div>
+                    <Button text="Log in" onClick={() => { }}
                     />
-            </div>
+                </div>
 
-            {/* Dark mode Toggle */}
-            <div className="flex flex-col w-full items-center">
-                <span className="font-nunito text-[16px] text-[#0A302F] font-medium">
-                    Dark mode
-                </span>
-                <ToggleSwitch />
+                {/* Dark mode Toggle */}
+                <div className="flex flex-col w-full items-center">
+                    <span className="font-nunito text-[16px] text-[#0A302F] font-medium">
+                        Dark mode
+                    </span>
+                    <ToggleSwitch />
+                </div>
             </div>
-        </div>
         </>
     );
 };
