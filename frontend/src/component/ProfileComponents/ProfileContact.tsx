23811cfa3775658d303677ba561d48be09c416e6
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-03-05 10:44:08 +0100

    new div

--- frontend/src/component/ProfileComponents/ProfileContact.tsx
+++ frontend/src/component/ProfileComponents/ProfileContact.tsx
@@ -2,10 +2,13 @@ export const ProfileContact = () => {
 
     return (
         <>
-            <label htmlFor="Email">Email:</label>
-            <input type="email" id="Email" />
-            <label htmlFor="tlf">Phone:</label>
-            <input type="tel" id="tlf" />
+            <div className="profile-contact">
+                <label htmlFor="Email">Email:</label>
+                <input type="email" id="Email" />
+                <label htmlFor="tlf">Phone:</label>
+                <input type="tel" id="tlf" />
+            </div>
+
         </>
     )
 }
