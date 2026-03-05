85817cc8ac749da0edff585db276e81c5843fc94
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-03-05 10:47:11 +0100

    simple contact

--- frontend/src/component/ProfileComponents/ProfileContact.tsx
+++ frontend/src/component/ProfileComponents/ProfileContact.tsx
@@ -1,12 +1,19 @@
-export const ProfileContact = () => {
+
+interface ProfileContactProps {
+    email: string;
+    phone: number;
+}
+
+export const ProfileContact = ({ email, phone }: ProfileContactProps) => {
+    
 
     return (
         <>
             <div className="profile-contact">
                 <label htmlFor="Email">Email:</label>
-                <input type="email" id="Email" />
+                <input type="email" id="Email" value={email} readOnly />
                 <label htmlFor="tlf">Phone:</label>
-                <input type="tel" id="tlf" />
+                <input type="tel" id="tlf" value={phone} readOnly />
             </div>
 
         </>
