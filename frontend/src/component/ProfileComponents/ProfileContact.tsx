2405f892a745ab95f7122172e2d33eddf5b89296
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-03-05 10:30:58 +0100

    simple contact not done

---
+++ frontend/src/component/ProfileComponents/ProfileContact.tsx
@@ -0,0 +1,11 @@
+export const ProfileContact = () => {
+
+    return (
+        <>
+            <label htmlFor="Email">Email:</label>
+            <input type="email" id="Email" />
+            <label htmlFor="tlf">Phone:</label>
+            <input type="tel" id="tlf" />
+        </>
+    )
+}
