9eebbc921cbb593887e3a6fe4aafc62e08942488
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-03-10 11:31:21 +0100

    Completed the components that could be found #61

--- frontend/src/pages/LoginPageView.tsx
+++ frontend/src/pages/LoginPageView.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../component/Header/Header";
-import Button from "../component/LoginSignInComponents/Button";
+import {Button} from "../component/Button/Button";
 import InputField from "../component/LoginSignInComponents/InputField";
 import ToggleSwitch from "../component/LoginSignInComponents/ToggleSwitch";
 
