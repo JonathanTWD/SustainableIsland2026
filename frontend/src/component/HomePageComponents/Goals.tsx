5663981f20c3f2b28f0a76e04494622a661341c9
jonathanelruso <jonathanmoreraapaza@gmail.com>
2026-03-11 10:28:23 +0000

    Refactor Goals component to fetch and display water savings metrics dynamically

--- frontend/src/component/HomePageComponents/Goals.tsx
+++ frontend/src/component/HomePageComponents/Goals.tsx
@@ -1,31 +1,48 @@
+import { useEffect, useState } from "react";
+import { metricsService } from "../../services/metrics.service";
 import { Description } from "../SubText/Description";
 import ProgressBar from "./ProgressBar";
 
 interface GoalsProps {
-    WaterSavedToday?: number;
-    WaterSavedTotal?: number;
-    Goal?: number;
-    Users?: number;
+  Users?: number;
 }
 
-const Goals = ({ WaterSavedToday, WaterSavedTotal, Goal, Users }: GoalsProps) => {
-    return (
-        <>
-            <div className="flex flex-col items-center">
-                <h2 className="font-kalam font-bold text-[40px]">
-                    Global Goals
-                </h2>
-                <div className="flex flex-col gap-4 items-center font-nunito font-medium text-[14px]">
-                    <Description text="Join a global community reducing water use." />
-                    <Description text="136 L of water saved today" />
-                    <Description text="100.000 L / 1.000.000 L  of water saved in 2026" />
-                    <ProgressBar />
-                    <Description text="430 people are saving water right now." />
-                </div>
+const Goals = ({ Users }: GoalsProps) => {
 
-            </div>
-        </>
-    );
-}
+  const [waterSavedToday, setWaterSavedToday] = useState("0");
+  const [waterSavedTotal, setWaterSavedTotal] = useState("0");
+  const [goals, setGoals] = useState("1.000.000");
+
+  useEffect(() => {
+    const fetchMetrics = async () => {
+      try {
+        const metrics = await metricsService.getTotalSaved();
+        setWaterSavedToday(metrics.saved_today_liters.toLocaleString());
+        setWaterSavedTotal(metrics.saved_year_liters.toLocaleString());
+      } catch (error) {
+        console.error("Something went wrong:", error);
+      }
+    };
+
+    fetchMetrics();
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h2 className="font-kalam font-bold text-[40px]">Global Goals</h2>
+        <div className="flex flex-col gap-4 items-center font-nunito font-medium text-[14px]">
+          <Description text="Join a global community reducing water use." />
+          <Description text={`${waterSavedToday} L of water saved today`} />
+          <Description
+            text={`${waterSavedTotal} L / ${goals} L  of water saved in 2026`}
+          />
+          <ProgressBar />
+          <Description text={`${Users} people are saving water right now.`} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Goals;
